86cdf860f87bd21d459f429fa0522f2e13a098e6
JorgeALozanoJ <jorlozjim@outlook.com>
2024-12-27 14:01:27 -0500

    DeletingConsoleLogs

--- src/app/pages/user-info/user-info.component.ts
+++ src/app/pages/user-info/user-info.component.ts
@@ -12,28 +12,21 @@ export class UserInfoComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, private router: Router) { }
   goBack(): void {
-    this.router.navigate(['/']);  // Aquí puedes poner la ruta a la que deseas volver
+    this.router.navigate(['/']); 
   }
 
   ngOnInit(): void {
     const storedUserInfo = localStorage.getItem('userInfo');
     
 
-    // Verificar si hay datos en localStorage
     if (storedUserInfo) {
-      this.userInfo = JSON.parse(storedUserInfo); // Convertir de string a objeto
+      this.userInfo = JSON.parse(storedUserInfo); 
     } else {
-      // Si no hay datos en localStorage, manejar el caso aquí, por ejemplo:
       console.log('No se encontraron datos de usuario en localStorage');
     }
 
-    // Obtener los parámetros de la URL (si es necesario)
     const documentId = this.route.snapshot.paramMap.get('documentId');
     const documentType = this.route.snapshot.paramMap.get('documentType');
-
-    // Opcional: Puedes usar documentId y documentType si los necesitas en esta página
-    console.log('Document ID:', documentId);
-    console.log('Document Type:', documentType);
     
   }
 }
